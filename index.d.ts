8588cb50abfe5eb75d37989fb2f639e143aa4b07
Brage Sekse Aarset <brage.aarset@gmail.com>
2022-10-18 09:41:16 +0300

    fix: use tabs

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module "norway-postal-codes" {
-		const postalCodes: Record<string, string>;
-		export default postalCodes;
+	const postalCodes: Record<string, string>;
+	export default postalCodes;
 }
